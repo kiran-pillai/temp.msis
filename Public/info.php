7d9292cc5367a771ad3a413f03810a45b0312e91
kpillai9623 <kpillai@indiana.edu>
2018-10-07 21:15:50 -0400

    d

---
+++ Public/info.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+<header>
+  <style>
+    header {
+      background-color: #add8e6;
+      padding: 1px;
+      text-align: center;
+      color: white;
+    }
+  </style>
+<nav>
+<center>
+<a href="kpillls_info.html">Info</a> |
+<a href="index.html">Home</a> |
+<a href="Future.html">Future</a> |
+</center>
+</nav>
+<p><h1 class="text-white">KPILLS KORNER</h1></p>
+</header>
+</head>
+  <div class="containter-fluid"
+  <body>
+  <div class="p-3 mb-2 bg-light text-dark">
+    <center><h2 class="text-dark">Future of Kpillls</h2></center>
+    <ol><li>Graduate from MSIS</li>
+        <li>Join a rotational program</li>
+        <li>Move to Houston, TX</li>
+        <li>Make a lot of money</li>
+        <li>Find a wife</li>
+        <li>Write a will</li>
+        <li>Befriend Eckhart Tolle</li>
+        <a target"_blank" href="img/eckhart.jpg">
+        <img src="img/eckhart.jpg" width="50" height="50"></a>
+    </ol>
+    Hire Me:
+    <a href="Documents/Resume.docx" download>Resume</a>
+    </div>
+  </div>
+  </body>
+</html>
